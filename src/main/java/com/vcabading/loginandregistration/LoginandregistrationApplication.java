ab2fa58a7627cba7926b566d27749f5a9e6c1592
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-10 12:08:02 -0800

    initial commit

---
+++ src/main/java/com/vcabading/loginandregistration/LoginandregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.vcabading.loginandregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginandregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginandregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcabading/loginandregistration/LoginandregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.loginandregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginandregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
