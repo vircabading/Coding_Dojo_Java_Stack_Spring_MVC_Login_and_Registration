f1c83090dfd01a79a2bf16b24b7142e490e960fc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-10 15:00:16 -0800

    add routes to User Controller

--- src/main/java/com/vcabading/loginandregistration/controllers/HomeController.java
+++ src/main/java/com/vcabading/loginandregistration/controllers/HomeController.java
@@ -1,16 +1,57 @@
 package com.vcabading.loginandregistration.controllers;
 
+package com.username.cafeJava.controllers;
+    
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-/////////////////////////////////////////////////////////////////
-//HOME CONTROLLER
-/////////////////////////////////////////////////////////////////
-
+import com.vcabading.loginandregistration.models.LoginUser;
+import com.vcabading.loginandregistration.models.User;
+import com.vcabading.loginandregistration.services.UserService;
+    
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String index() {
-		return "index.jsp";
-	}
+    
+    @Autowired
+    private UserService userServ;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+    
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        userServ.register(newUser, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        session.setAttribute("user_id", newUser.getId());
+        return "redirect:/home";
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        User user = userServ.login(newLogin, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+        session.setAttribute("user_id", user.getId());
+        return "redirect:/home";
+    }
+    
 }
