6ddef771db04d4dfb640797287cb6a378814373f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-10 13:21:12 -0800

    add UserRepository

---
+++ src/main/java/com/vcabading/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.vcabading.loginandregistration.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vcabading.loginandregistration.models.User;
+
+/////////////////////////////////////////////////////////////
+//	USER REPOSITORY
+/////////////////////////////////////////////////////////////
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    Optional<User> findByEmail(String email);
+    
+}
