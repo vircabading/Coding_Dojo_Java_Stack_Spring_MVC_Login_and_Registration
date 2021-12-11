82ab092d8a70eb74e19ece2f14bb137ab996265b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-10 23:35:03 -0800

    add check if userName is already in database on registration attempt

--- src/main/java/com/vcabading/loginandregistration/services/UserService.java
+++ src/main/java/com/vcabading/loginandregistration/services/UserService.java
@@ -30,9 +30,8 @@ public class UserService {
         if(userRepo.findByEmail(newUser.getEmail()).isPresent()) {		// Check if E-mail is already in database
             result.rejectValue("email", "Unique", "This email is already in use!");
         }
-        if(userRepo  
-        		b.findByEmail(newUser.getEmail()).isPresent()) {		// Check if E-mail is already in database
-            result.rejectValue("email", "Unique", "This email is already in use!");
+        if(userRepo.findByUserName(newUser.getUserName()).isPresent()) {
+            result.rejectValue("userName", "Unique", "This Name is already in use!");
         }
         if(!newUser.getPassword().equals(newUser.getConfirm())) {		// Check to make sure password matches confirm password
             result.rejectValue("confirm", "Matches", "The Confirm Password must match Password!");
